8bf9a11635708707491cbbf4a124273acb374b56
Dion Roloff <dion.roloff@gmail.com>
2019-02-06 13:36:47 -0600

    base goals reached

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,27 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+
+    if (req.isAuthenticated()) {
+        // They were authenticated! User may do the next thing
+        // Note! They may not be Authorized to do all things
+        console.log('req.user:', req.user);
+        const queryText = `SELECT * FROM "secret"
+                           WHERE "secrecy_level" < $1;`;
+        pool.query(queryText, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+        // res.send(req.user);
+      } else {
+        // They are not authenticated.
+        res.sendStatus(403);
+      }
+
+
+    
 });
 
 module.exports = router;
